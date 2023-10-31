a40e0c6204efd96e2c1d79f8a037a68d31ba4c1b
Yasmin Castro <yasminsdcastro@gmail.com>
2023-10-31 17:30:34 -0300

    feat: personal info

--- components/LeftSideBar/ContactInfo/ContactInfo.tsx
+++ components/LeftSideBar/ContactInfo/ContactInfo.tsx
@@ -1,10 +1,11 @@
 import { Avatar } from "flowbite-react";
 import { AiOutlineMail } from "react-icons/ai";
 import { BiLogoLinkedin } from "react-icons/bi";
+import { FiGithub } from "react-icons/fi";
 
 const ContactInfo = () => {
   return (
-    <div className="flex flex-col items-center space-y-3">
+    <div className="flex flex-col items-center space-y-3 mb-6">
       <Avatar
         alt="avatar of Yasmin Castro"
         img="https://avatars.githubusercontent.com/u/62770317"
@@ -27,6 +28,14 @@ const ContactInfo = () => {
           className="bg-lightBlue-600 p-2 rounded-full hover:bg-lightBlue-700"
         >
           <AiOutlineMail className="text-lg" />
+        </a>
+        <a
+          href="https://github.com/YasminCastro"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-lightBlue-600 p-2 rounded-full hover:bg-lightBlue-700"
+        >
+          <FiGithub className="text-lg" />
         </a>
       </div>
     </div>
--- components/LeftSideBar/SideBar.tsx
+++ components/LeftSideBar/SideBar.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ContactInfo from "./ContactInfo/ContactInfo";
+import PersonalInfo from "./PersonalInfo/PersonalInfo";
 
 const LeftSidebar = () => {
   return (
     <div className="p-4">
       <ContactInfo />
+      <PersonalInfo />
     </div>
   );
 };
