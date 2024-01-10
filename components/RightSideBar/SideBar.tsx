1d9bc0d511676efcaca672197b892495a9495d37
Yasmin Castro <yasminsdcastro@gmail.com>
2024-01-10 12:42:00 -0300

    refactor: side bar

--- components/RightSideBar/SideBar.tsx
+++ components/RightSideBar/SideBar.tsx
@@ -1,15 +1,23 @@
 import React from "react";
-import { FaHome } from "react-icons/fa";
-import { MdSchool } from "react-icons/md";
+import { FaHome, FaBriefcase, FaGraduationCap } from "react-icons/fa";
 
 const RightSidebar = () => {
   return (
-    <div className="flex flex-col space-y-10 items-center justify-center h-screen">
-      <button>
-        <FaHome />
+    <div className="flex flex-col space-y-6 items-center justify-center h-screen">
+      <button className="bg-secondary p-3 rounded-full hover:bg-primary-foreground">
+        <FaHome className="text-xl" />
       </button>
-      <button>
-        <MdSchool />
+
+      <button className="bg-secondary p-3 rounded-full hover:bg-primary-foreground">
+        <FaGraduationCap className="text-xl" />
+      </button>
+
+      <button className="bg-secondary p-3 rounded-full hover:bg-primary-foreground">
+        <FaBriefcase className="text-lg" />
+      </button>
+
+      <button className="bg-secondary p-3 rounded-full hover:bg-primary-foreground ">
+        <p className="text-sm font-bold">CV</p>
       </button>
     </div>
   );
