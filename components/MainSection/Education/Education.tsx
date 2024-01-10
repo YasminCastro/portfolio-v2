f19583c3e233c75501155ea8e79783c83177982e
Yasmin Castro <yasminsdcastro@gmail.com>
2024-01-10 14:37:27 -0300

    refactor: institution

--- components/MainSection/Education/Education.tsx
+++ components/MainSection/Education/Education.tsx
@@ -5,29 +5,44 @@ const EducationSection = () => {
     <div className=" bg-white shadow-lg rounded-xl p-16">
       <h2 className="font-bold text-3xl w-fit">Educação</h2>
       <div className="space-y-3 mt-10">
-        <Education />
+        <Institution
+          institution="Instituto Federal de Goiás"
+          date="2020 - presente"
+          name="Bacharelado em Sistemas de Informação"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+          rerum, odit voluptate, suscipit fugiat sapiente debitis officiis
+          tempora nemo aliquid voluptatum possimus! Enim saepe, voluptatibus
+          adipisci sed possimus nesciunt unde."
+        />
       </div>
     </div>
   );
 };
 
-const Education = () => {
+interface IInstitution {
+  institution: string;
+  date: string;
+  name: string;
+  description: string;
+}
+
+const Institution = ({
+  institution,
+  date,
+  name,
+  description,
+}: IInstitution) => {
   return (
     <div className="flex justify-between w-full">
       <div className="space-y-2">
-        <h3 className="font-bold text-lg">Instituto Federal de Goiás</h3>
+        <h3 className="font-bold text-lg">{institution}</h3>
         <div>
-          <Badge>2020 - presente</Badge>
+          <Badge>{date}</Badge>
         </div>
       </div>
       <div className="space-y-6">
-        <p className="font-bold">Bacharelado em Sistemas de Informação</p>
-        <p className="max-w-lg">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
-          rerum, odit voluptate, suscipit fugiat sapiente debitis officiis
-          tempora nemo aliquid voluptatum possimus! Enim saepe, voluptatibus
-          adipisci sed possimus nesciunt unde.
-        </p>
+        <p className="font-bold">{name}</p>
+        <p className="max-w-lg">{description}</p>
       </div>
     </div>
   );
