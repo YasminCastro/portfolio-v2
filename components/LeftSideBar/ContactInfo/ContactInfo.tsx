44977b5b9135d1eea96e3ee959541f7955f012e6
Yasmin Castro <yasminsdcastro@gmail.com>
2024-01-10 09:44:41 -0300

    feat: shadcn

--- components/LeftSideBar/ContactInfo/ContactInfo.tsx
+++ components/LeftSideBar/ContactInfo/ContactInfo.tsx
@@ -1,4 +1,5 @@
-import { Avatar } from "flowbite-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
 import { AiOutlineMail } from "react-icons/ai";
 import { BiLogoLinkedin } from "react-icons/bi";
 import { FiGithub } from "react-icons/fi";
@@ -6,36 +7,38 @@ import { FiGithub } from "react-icons/fi";
 const ContactInfo = () => {
   return (
     <div className="flex flex-col items-center space-y-1 mb-6">
-      <Avatar
-        alt="avatar of Yasmin Castro"
-        img="https://avatars.githubusercontent.com/u/62770317"
-        rounded
-        size="lg"
-      />
+      <Avatar className="w-24 h-24">
+        <AvatarImage
+          src="https://avatars.githubusercontent.com/u/62770317"
+          alt="Yasmin Castro"
+        />
+        <AvatarFallback>YC</AvatarFallback>
+      </Avatar>
+
       <h1 className="text-lg font-bold">Yasmin Castro</h1>
-      <h2 className="text-lg  text-gray-600">Full-Stack Developer</h2>
+      <h2 className="text-base  text-gray-600">Full-Stack Developer</h2>
       <div className="flex gap-4">
         <a
           href="https://www.linkedin.com/in/yasmin-sdcastro/"
           target="_blank"
           rel="noopener noreferrer"
-          className="bg-lightBlue-600 p-2 rounded-full hover:bg-lightBlue-700"
+          className="bg-primary p-2 rounded-full hover:bg-primary-foreground"
         >
-          <BiLogoLinkedin className="text-lg" />
+          <BiLogoLinkedin className="text-lg text-secondary hover:text-secondary-foreground" />
         </a>
         <a
           href="mailto:yasminsdcastro@gmail.com"
-          className="bg-lightBlue-600 p-2 rounded-full hover:bg-lightBlue-700"
+          className="bg-primary p-2 rounded-full hover:bg-primary-foreground"
         >
-          <AiOutlineMail className="text-lg" />
+          <AiOutlineMail className="text-lg text-secondary hover:text-secondary-foreground" />
         </a>
         <a
           href="https://github.com/YasminCastro"
           target="_blank"
           rel="noopener noreferrer"
-          className="bg-lightBlue-600 p-2 rounded-full hover:bg-lightBlue-700"
+          className="bg-primary p-2 rounded-full hover:bg-primary-foreground"
         >
-          <FiGithub className="text-lg" />
+          <FiGithub className="text-lg text-secondary hover:text-secondary-foreground" />
         </a>
       </div>
     </div>
