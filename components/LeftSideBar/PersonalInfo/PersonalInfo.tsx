1a5c4b14583ce323fb619ab6763a9e24ffda7dd6
Yasmin Castro <yasminsdcastro@gmail.com>
2024-01-10 09:51:12 -0300

    refactor: personal info

--- components/LeftSideBar/PersonalInfo/PersonalInfo.tsx
+++ components/LeftSideBar/PersonalInfo/PersonalInfo.tsx
@@ -1,12 +1,23 @@
 const PersonalInfo = () => {
   return (
-    <div className="flex flex-col items-center space-y-3 border-t-2 border-gray-200 py-6">
-      <div className="flex justify-between items-center w-full">
-        <p className="bg-lightBlue-300 px-2 rounded-md">Endereço:</p>
-        <p>Goiânia - GO</p>
-      </div>
+    <div className="flex flex-col items-center space-y-3 border-t-2 border-gray-200 py-4">
+      <PersonalCard title="Endereço" description="Goiânia - GO" />
     </div>
   );
 };
 
 export default PersonalInfo;
+
+interface IPersonalCard {
+  title: string;
+  description: string;
+}
+
+const PersonalCard = ({ title, description }: IPersonalCard) => {
+  return (
+    <div className="flex justify-between items-center w-full">
+      <p className="bg-primary px-2 rounded-md text-secondary">{title}:</p>
+      <p>{description}</p>
+    </div>
+  );
+};
