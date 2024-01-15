cb30e6cedb7c10e8bf81469ac41cf8a4d4afe574
Yasmin Castro <yasminsdcastro@gmail.com>
2024-01-15 18:02:39 -0300

    refactor: small fonts

--- components/RightSideBar/SideBar.tsx
+++ components/RightSideBar/SideBar.tsx
@@ -9,40 +9,40 @@ import {
 
 const RightSidebar = () => {
   return (
-    <div className="flex h-screen flex-col items-center justify-center space-y-6">
+    <div className="flex h-screen flex-col items-center justify-center space-y-6 max-lg:space-y-5">
       <a
-        className="rounded-full bg-secondary p-3 hover:bg-primary-foreground"
+        className="rounded-full bg-secondary p-3 hover:bg-primary-foreground  max-lg:p-2"
         href="#"
       >
-        <FaHome className="text-xl" />
+        <FaHome className="text-xl  max-lg:text-base" />
       </a>
       <a
-        className="rounded-full bg-secondary p-3 hover:bg-primary-foreground"
+        className="rounded-full bg-secondary p-3 hover:bg-primary-foreground max-lg:p-2"
         href="#education"
       >
-        <FaGraduationCap className="text-xl" />
+        <FaGraduationCap className="text-xl max-lg:text-lg" />
       </a>
       <a
-        className="rounded-full bg-secondary p-3 hover:bg-primary-foreground"
+        className="rounded-full bg-secondary p-3 hover:bg-primary-foreground max-lg:p-2"
         href="#work"
       >
-        <FaBriefcase className="text-lg" />
+        <FaBriefcase className="text-lg max-lg:text-base" />
       </a>
       <a
-        className="rounded-full bg-secondary p-3 hover:bg-primary-foreground"
+        className="rounded-full bg-secondary p-3 hover:bg-primary-foreground max-lg:p-2"
         href="#projects"
       >
-        <FaBug className="text-lg" />
+        <FaBug className="text-lg max-lg:text-base" />
       </a>
       <a
-        className="rounded-full bg-secondary p-3 hover:bg-primary-foreground"
+        className="rounded-full bg-secondary p-3 hover:bg-primary-foreground max-lg:p-2"
         href="#contact"
       >
-        <FaRegUser className="text-lg" />
+        <FaRegUser className="text-lg max-lg:text-base" />
       </a>
-      <button className="rounded-full bg-secondary p-3 hover:bg-primary-foreground ">
+      <button className="rounded-full bg-secondary p-3 hover:bg-primary-foreground max-lg:p-2 ">
         <a
-          className="text-sm font-bold"
+          className="text-sm font-bold max-lg:text-xs"
           href="/YasminCV-BR.pdf"
           target="_blank"
         >
