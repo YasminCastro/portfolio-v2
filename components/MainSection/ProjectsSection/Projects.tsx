edbcbc3385e40c8242327a8e3cf8984e1424f635
Yasmin Castro <yasminsdcastro@gmail.com>
2024-01-15 18:09:34 -0300

    refactor: projects

--- components/MainSection/ProjectsSection/Projects.tsx
+++ components/MainSection/ProjectsSection/Projects.tsx
@@ -5,9 +5,9 @@ const ProjectsSection = () => {
   return (
     <div id="projects" className="space-y-8">
       <div className="flex justify-center">
-        <h2 className="text-3xl font-bold">Projetos</h2>
+        <h2 className="text-3xl font-bold max-lg:text-2xl">Projetos</h2>
       </div>
-      <div className=" p-16">
+      <div className=" p-16 max-lg:p-6">
         <div className="grid grid-cols-3 place-items-center gap-3">
           {projects.map((project, index) => {
             return <Project project={project} key={`project-${index}`} />;
