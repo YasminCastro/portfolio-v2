e1d6b260fc04ed5036374b3a664365a813997b4d
Yasmin Castro <yasminsdcastro@gmail.com>
2024-01-15 17:30:08 -0300

    feat: about

--- components/MainSection/HeroSection/HeroSection.tsx
+++ components/MainSection/HeroSection/HeroSection.tsx
@@ -1,8 +1,11 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Image from "next/image";
+import { differenceInYears } from "date-fns";
 
 const HeroSection = () => {
+  const initialDate = new Date(2021, 9);
+  const today = new Date();
   return (
     <div className="flex items-center justify-between rounded-xl bg-white px-16 py-3 shadow-lg">
       <div className="space-y-3">
@@ -11,9 +14,11 @@ const HeroSection = () => {
           <a className="text-primary">Full-Stack</a>
         </h1>
 
-        <p className="w-2/3 max-w-xl">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam
-          dignissimos accusantium odit aperiam, dolor soluta dolorem?
+        <p className="w-3/4 max-w-xl text-justify">
+          Tenho mais de {differenceInYears(today, initialDate)} anos de
+          experiência na área de desenvolvimento. Me destaco por ser prática,
+          eficaz e por aprender tudo muito rápido. Sou apaixonada por tecnologia
+          em geral e sempre busco me manter atualizada.
         </p>
 
         <Button asChild size="lg" className="w-fit">
