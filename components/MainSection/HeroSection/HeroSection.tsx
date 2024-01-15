041223c1e90ff1a61dbb43390a288cb4d2e8d446
Yasmin Castro <yasminsdcastro@gmail.com>
2024-01-15 17:30:50 -0300

    refactor: responsividade

--- components/MainSection/HeroSection/HeroSection.tsx
+++ components/MainSection/HeroSection/HeroSection.tsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
   const initialDate = new Date(2021, 9);
   const today = new Date();
   return (
-    <div className="flex items-center justify-between rounded-xl bg-white px-16 py-3 shadow-lg">
+    <div className="flex items-center justify-around rounded-xl bg-white px-16 py-3 shadow-lg">
       <div className="space-y-3">
         <h1 className="text-4xl font-bold">
           Eu sou a Yasmin, <br /> Desenvolvedora{" "}
