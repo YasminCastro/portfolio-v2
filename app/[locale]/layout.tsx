36881b254f9cd5e02a1bec364882f2aca21f5200
Yasmin Castro <yasminsdcastro@gmail.com>
2024-04-04 21:12:30 -0300

    feat: client component

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -4,6 +4,8 @@ import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { ThemeProvider } from "@/components/theme-provider";
+import { unstable_setRequestLocale } from "next-intl/server";
+import { NextIntlClientProvider, useMessages } from "next-intl";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +21,8 @@ export default function RootLayout({
   children: React.ReactNode;
   params: { locale: string };
 }) {
+  unstable_setRequestLocale(locale);
+  const messages = useMessages();
   return (
     <html lang={locale}>
       <body className={inter.className}>
@@ -28,7 +32,10 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children} <Analytics /> <SpeedInsights />
+          <NextIntlClientProvider messages={messages}>
+            {children}
+          </NextIntlClientProvider>
+          <Analytics /> <SpeedInsights />
         </ThemeProvider>
       </body>
     </html>
