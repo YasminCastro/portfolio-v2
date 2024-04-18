7addb35df6d235afc5b521587c1a952fcacc8b69
Yasmin Castro <yasminsdcastro@gmail.com>
2024-04-17 21:21:35 -0300

    feat: languages translation

--- components/LeftSideBar/Languages/Languages.tsx
+++ components/LeftSideBar/Languages/Languages.tsx
@@ -3,13 +3,12 @@ import { useTranslations } from "next-intl";
 
 const Languages = () => {
   const t = useTranslations("Languages");
-  console.log(t("languages"));
   return (
     <div className=" py-3">
       <h2 className="text-lg font-bold max-lg:text-base">{t("languages")}</h2>
       <div className="my-3 space-y-4">
-        <LanguageProgess languageName="Português" value={100} />
-        <LanguageProgess languageName="Inglês" value={80} />
+        <LanguageProgess languageName={t("portuguese")} value={100} />
+        <LanguageProgess languageName={t("english")} value={80} />
       </div>
     </div>
   );
