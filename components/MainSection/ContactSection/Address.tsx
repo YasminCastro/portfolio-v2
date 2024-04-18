2d2b7fe2eec9828b7cdcc4a25ad2d4d1cf14f597
Yasmin Castro <yasminsdcastro@gmail.com>
2024-04-17 22:04:20 -0300

    feat: translation contact

--- components/MainSection/ContactSection/Address.tsx
+++ components/MainSection/ContactSection/Address.tsx
@@ -1,6 +1,9 @@
 import { FaMapLocationDot } from "react-icons/fa6";
+import { useTranslations } from "next-intl";
 
 const Address = () => {
+  const t = useTranslations("ContactSection");
+
   return (
     <div className="flex min-h-full w-80 flex-col items-center justify-around rounded-xl bg-card p-7 max-lg:p-4 max-md:w-full">
       <div className="w-fit rounded-full bg-primary-foreground p-3 dark:bg-secondary ">
@@ -8,11 +11,11 @@ const Address = () => {
       </div>
       <div className="w-full space-y-1 pt-5 text-gray-600 dark:text-gray-100">
         <div className="flex justify-between ">
-          <p className="font-bold">País:</p>
-          <p>Brasil</p>
+          <p className="font-bold">{t("country")}:</p>
+          <p>{t("brazil")}</p>
         </div>
         <div className="flex justify-between">
-          <p className="font-bold">Cidade:</p>
+          <p className="font-bold">{t("city")}:</p>
           <p>Goiânia - GO</p>
         </div>
       </div>
--- components/MainSection/ContactSection/Contact.tsx
+++ components/MainSection/ContactSection/Contact.tsx
@@ -1,11 +1,14 @@
 import Address from "./Address";
 import Usernames from "./Usernames";
+import { useTranslations } from "next-intl";
 
 const ContactSection = () => {
+  const t = useTranslations("ContactSection");
+
   return (
     <div id="contact" className="space-y-8">
       <div className="flex justify-center">
-        <h2 className="text-3xl font-bold max-lg:text-2xl">Contato</h2>
+        <h2 className="text-3xl font-bold max-lg:text-2xl">{t("contact")}</h2>
       </div>
       <div className="flex justify-around gap-3 max-[920px]:flex-col max-[920px]:items-center">
         <Address />
