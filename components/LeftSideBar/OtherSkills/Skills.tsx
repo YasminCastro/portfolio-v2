671d0e712c8b01e8191af73b3ac23bcb19e8d638
Yasmin Castro <yasminsdcastro@gmail.com>
2024-04-17 21:22:36 -0300

    feat: other skillks translation

--- components/LeftSideBar/OtherSkills/Skills.tsx
+++ components/LeftSideBar/OtherSkills/Skills.tsx
@@ -1,9 +1,11 @@
 import { MdOutlineFiberSmartRecord } from "react-icons/md";
+import { useTranslations } from "next-intl";
 
 const OtherSkills = () => {
+  const t = useTranslations("OtherSkills");
   return (
     <div className="py-3">
-      <h2 className="text-lg font-bold max-lg:text-base">Outras Skills</h2>
+      <h2 className="text-lg font-bold max-lg:text-base">{t("otherSkills")}</h2>
       <div className="my-3 space-y-4">
         <Skill skill="Git" />
         <Skill skill="AWS" />
